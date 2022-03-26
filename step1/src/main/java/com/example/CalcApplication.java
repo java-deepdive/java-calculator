51e2f27c851ac21b4f7dc70e295f13bcfd8573a8
seokrae <kslbsh@gmail.com>
2022-03-26 22:07:07 +0900

    feat: 프로젝트 생성

---
+++ step1/src/main/java/com/example/CalcApplication.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class CalcApplication {
+
+	public static void main(String[] args) {
+		System.out.println("계산기 프로그램 시작");
+		System.out.println("계산기 프로그램 종료");
+	}
+}
