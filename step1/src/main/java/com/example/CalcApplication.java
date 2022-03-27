5de37756a81ed3e2a67baa763e49f63dd979a6d1
seokrae <kslbsh@gmail.com>
2022-03-27 23:57:35 +0900

    feat: 연산 메뉴 추가

--- step1/src/main/java/com/example/CalcApplication.java
+++ step1/src/main/java/com/example/CalcApplication.java
@@ -1,9 +1,29 @@
 package com.example;
 
+import com.example.application.Calculator;
+
+import java.util.Scanner;
+
 public class CalcApplication {
 
 	public static void main(String[] args) {
 		System.out.println("계산기 프로그램 시작");
+		Calculator calculator = new Calculator();
+
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("메뉴 1. 계산, 2. 종료");
+			String flag = scanner.nextLine();
+			if("2".equals(flag)) {
+				break;
+			}
+			System.out.print("연산 식 입력: ");
+			String line = scanner.nextLine();
+			String calculate = calculator.calculate(line);
+			System.out.println("calculate = " + calculate);
+		}
+
 		System.out.println("계산기 프로그램 종료");
 	}
 }
